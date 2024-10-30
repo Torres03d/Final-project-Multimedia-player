b4a56c5df7891a1f4249a665ff6f8c6a1ff462cf
Victor R <victor.rojas@utp.edu.co>
2024-10-30 04:18:35 -0500

    header file with documentation

---
+++ mainwindow.h
@@ -0,0 +1,53 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtMultimedia>
+#include <QtMultimediaWidgets>
+#include <QtCore>
+#include <QtWidgets>
+#include <QtGui>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void durationChanged(quint64 duration); // Llamada cuando la duración cambia
+    void positionChanged(quint64 duration); // Llamada cuando la posición cambia
+    void on_pushButton_Play_clicked(); // Maneja la reproducción
+    void on_pushButton_Stop_clicked(); // Maneja el botón de stop
+    void on_pushButton_Volume_clicked(); // Cambia el volumen
+    void on_horizontalSlider_Volume_valueChanged(int value); // Cambia la posición de reproducción
+    void on_horizontalSlider_Duration_valueChanged(int value); // Cambia duracion reproducción
+    void on_action_Open_triggered(); // Abre el diálogo de archivo
+    void on_pushButton_Seek_Backward_clicked(); // Retrocede 10 segundos
+    void on_pushButton_Seek_Forward_clicked(); // Avanza 10 segundos
+
+private:
+    Ui::MainWindow *ui; // Interfaz de usuario
+    QMediaPlayer *Player; // Reproductor de medios
+    QVideoWidget *Video; // Widget para mostrar video
+    QAudioOutput *audioOutput; // Salida de audio
+    quint64 mDuration; // Duración del archivo
+    bool IS_Pause = true; // Estado de pausa
+    bool IS_Muted = false; // Estado de mute
+    void updateDuration(quint64 Duration); // Actualiza la duracion
+    void updateCurrentTime(qint64 current); // Actualiza el tiempo actual
+    void updateTotalTime(qint64 total); // Actualiza el tiempo total
+
+    QLabel *mediaTitleLabel;
+    QVideoWidget *videoWidget;
+    QLabel *albumArtLabel;
+    void displayMedia(const QString& fileName, bool isVideo);
+};
+
+#endif // MAINWINDOW_H
