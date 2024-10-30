08ce0311fa0f757035b3cfa45b941dc78e1a9c56
Victor R <victor.rojas@utp.edu.co>
2024-10-30 04:17:05 -0500

    Update mainwindow.cpp
    code documentation

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,32 +10,34 @@ MainWindow::MainWindow(QWidget *parent)
     audioOutput = new QAudioOutput();
     Player->setAudioOutput(audioOutput);
 
-    // Initialize video widget
+    // Inicializacion video widget
     videoWidget = new QVideoWidget(this);
     videoWidget->setGeometry(ui->groupBox_Display->geometry());
     videoWidget->hide();
 
-    // Initialize album art label
+    // Inicializacion album art label
     albumArtLabel = new QLabel(this);
     albumArtLabel->setGeometry(ui->groupBox_Display->geometry());
     albumArtLabel->setAlignment(Qt::AlignCenter);
     albumArtLabel->hide();
 
-    // Initialize media title label
+    // Inicializacion media title label
     mediaTitleLabel = ui->label_Title;
 
-    // Connect signals to slots
+    // Señales de Coneccion para los slots
     connect(Player, &QMediaPlayer::durationChanged, this, &MainWindow::durationChanged);
     connect(Player, &QMediaPlayer::positionChanged, this, &MainWindow::positionChanged);
 
 
 }
 
+// Destructor de la clase MainWindow
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+// Función para cuando la duración del medio cambia
 void MainWindow::durationChanged(quint64 duration)
 {
     mDuration = duration / 1000;
@@ -43,6 +45,7 @@ void MainWindow::durationChanged(quint64 duration)
     updateTotalTime(mDuration);
 }
 
+// Función para cuando la posición del medio cambia
 void MainWindow::positionChanged(quint64 duration)
 {
     if (!ui->horizontalSlider_Duration->isSliderDown())
@@ -52,6 +55,7 @@ void MainWindow::positionChanged(quint64 duration)
     updateCurrentTime(duration / 1000);
 }
 
+// Funcion para el contador de tiempo
 void MainWindow::updateCurrentTime(qint64 current)
 {
     QTime currentTime((current / 3600) % 60, (current / 60) % 60, (current % 60));
@@ -59,6 +63,7 @@ void MainWindow::updateCurrentTime(qint64 current)
     ui->label_Current_Time->setText(currentTime.toString(format));
 }
 
+// Funcion para el tiempo total
 void MainWindow::updateTotalTime(qint64 total)
 {
     QTime totalTime((total / 3600) % 60, (total / 60) % 60, (total % 60));
@@ -66,12 +71,19 @@ void MainWindow::updateTotalTime(qint64 total)
     ui->label_Total_Time->setText(totalTime.toString(format));
 }
 
-void MainWindow::on_actionOpen_triggered()
+// Abre el buscador de archivos para seleccionar un archivo multimedia
+void MainWindow::on_action_Open_triggered()
 {
-    QString fileName = QFileDialog::getOpenFileName(this, tr("Select the File"), "", tr("Media Files (*.mp3 *.mp4)"));
-    displayMedia(fileName, fileName.endsWith(".mp4"));
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Media"), "",
+                                                    tr("Multimedia Files (*.mp3 *.mp4 *.wav *.avi)"));
+    if (fileName.isEmpty())
+        return;
+
+    bool isVideo = fileName.endsWith(".mp4") || fileName.endsWith(".avi");
+    displayMedia(fileName, isVideo);
 }
 
+// Funcion para mostrar el archivo multimedia
 void MainWindow::displayMedia(const QString& fileName, bool isVideo)
 {
     if (isVideo)
@@ -92,6 +104,7 @@ void MainWindow::displayMedia(const QString& fileName, bool isVideo)
     mediaTitleLabel->setText(QFileInfo(fileName).baseName());
 }
 
+ // Maneja el click en el botón de play
 void MainWindow::on_pushButton_Play_clicked()
 {
     if (IS_Pause == true)
@@ -108,11 +121,13 @@ void MainWindow::on_pushButton_Play_clicked()
     }
 }
 
+ // Maneja el click en el botón de stop
 void MainWindow::on_pushButton_Stop_clicked()
 {
     Player->stop();
 }
 
+// Maneja el boton en el volumen
 void MainWindow::on_pushButton_Volume_clicked()
 {
     if (IS_Muted == false)
@@ -129,22 +144,26 @@ void MainWindow::on_pushButton_Volume_clicked()
     }
 }
 
+// Maneja el cambio en la posición de reproducción
 void MainWindow::on_horizontalSlider_Volume_valueChanged(int value)
 {
-    audioOutput->setVolume(value / 100.0);  // Adjust volume
+    audioOutput->setVolume(value / 100.0);  // Ajusta el volumen
 }
 
+// Maneja el cambio en el volumen
 void MainWindow::on_horizontalSlider_Duration_valueChanged(int value)
 {
     Player->setPosition(value * 1000);
 }
 
+// Retrocede la reproducción
 void MainWindow::on_pushButton_Seek_Backward_clicked()
 {
     ui->horizontalSlider_Duration->setValue(ui->horizontalSlider_Duration->value() - 20);
     Player->setPosition(ui->horizontalSlider_Duration->value() * 1000);
 }
 
+// Avanza la reproducción
 void MainWindow::on_pushButton_Seek_Forward_clicked()
 {
     ui->horizontalSlider_Duration->setValue(ui->horizontalSlider_Duration->value() + 20);
