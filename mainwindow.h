4f766aedb22780133c7d38ee3cc4f3abefe95687
Victor R <victor.rojas@utp.edu.co>
2024-11-01 13:40:21 -0500

    Update mainwindow.h
    new slots for the volume bar

--- mainwindow.h
+++ mainwindow.h
@@ -20,14 +20,20 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    bool eventFilter(QObject *watched, QEvent *event) override;
+    void leaveEvent(QEvent *event) override;
+
 private slots:
+    void showVolumeSlider(); //LLamada para mostrar la barra de volumen
+    void hideVolumeSlider(); //LLamada para ocultar la barra de volumen
     void on_horizontalSlider_Duration_sliderPressed(); // Llamada cuando se cambia la barra de duracion
     void on_horizontalSlider_Duration_sliderReleased(); // Llamada ajustar el audio al precionar barra de duracion
-    void updateTime(); // stot para actializar el contador
+    void updateTime(); // Slot para actualizar el contador
     void durationChanged(quint64 duration); // Llamada cuando la duración cambia
     void positionChanged(quint64 duration); // Llamada cuando la posición cambia
     void on_pushButton_Play_clicked(); // Maneja la reproducción
-    void on_pushButton_Stop_clicked(); // Maneja el botón de stop
+    void on_pushButton_Repeat_clicked(); // Maneja el botón de repeticion
     void on_pushButton_Volume_clicked(); // Cambia el volumen
     void on_horizontalSlider_Volume_valueChanged(int value); // Cambia la posición de reproducción
     void on_horizontalSlider_Duration_valueChanged(int value); // Cambia duración reproducción
@@ -40,19 +46,17 @@ private:
     Ui::MainWindow *ui; // Interfaz de usuario
     bool isUpdatingSlider; // flag
     QMediaPlayer *Player; // Reproductor de medios
-    QVideoWidget *Video; // Widget para mostrar video
+    QVideoWidget *videoWidget; // Widget para mostrar video
     QAudioOutput *audioOutput; // Salida de audio
     quint64 mDuration; // Duración del archivo
-    bool IS_Pause = true; // Estado de pausa
-    bool IS_Muted = false; // Estado de mute
+    bool IS_Pause; // Estado de pausa
+    bool IS_Muted; // Estado de mute
     QTimer *timeUpdateTimer; // Timer para actualizar el tiempo
-    void updateDuration(quint64 Duration); // Actualiza la duración
-    void updateCurrentTime(qint64 current); // Actualiza el tiempo actual
-    void updateTotalTime(qint64 total); // Actualiza el tiempo total
     QLabel *mediaTitleLabel;
-    QVideoWidget *videoWidget;
     QLabel *albumArtLabel;
     void displayMedia(const QString& fileName, bool isVideo);
+    void updateCurrentTime(qint64 current); // Declaración de la función updateCurrentTime
+    void updateTotalTime(qint64 total); // Declaración de la función updateTotalTime
 };
 
 #endif // MAINWINDOW_H
